687118842682d8823d62380ca1aaebceca831eb4
Darren Laderas <darren.laderas@ninthdesign.com>
2015-06-02 14:13:51 +0800

    js file revised

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-( function() {
-    var d = document,
-        b = document.body;
+var text = document.getElementById('textValue');
+var submit = document.getElementById('submitValue');
 
-    // Create Elements
-    // var div = document.createElement('box'),
-    //     div2 = document.createElement('inner-content-box')
-    //     div3 = document.createElement('form-wrapper');
+submit.addEventListener('click', function(e) {
+    e.preventDefault();
 
-    // Append Child
-    // body.appendChild(div)
-
-    var getValue = function () {
-        var text = document.getElementById('textValue');
-        var submit = document.getElementById('submitValue');
-
-        if ( textValue.value === '' ) {
-            alert ("Please enter a Value");
-            return false;
-        }
-
-        if ( textValue.value < 201 ) {
-            alert (textValue.value);
-            return false;
-        }
+    if ( textValue.value === '' ) {
+        alert ("Please enter a Value");
+        return false;
     }
-} )
+
+    if ( textValue.value == 0 ) {
+        alert ("Invalid Input, Try to put a value between 1 to 200 only");
+        return false;
+    }
+
+    if ( textValue.value <= 200 ) {
+        var getValue = document.getElementById('inserted-value');
+        getValue.appendChild(document.createTextNode('$' + textValue.value));
+    }
+
+    if ( textValue.value >= 201 ) {
+        alert('Invalid Input, Try to put a value between 1 to 200 only');
+        return false;
+    }
+}, false)
