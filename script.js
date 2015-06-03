33581ecb96fed58670ea31f4cbdbce059036828e
Darren Laderas <darren.laderas@ninthdesign.com>
2015-06-03 08:31:11 +0800

    updated script file

--- script.js
+++ script.js
@@ -2,25 +2,13 @@ var text = document.getElementById( 'textValue' );
 var submit = document.getElementById( 'submitValue' );
 var getValue = document.getElementById( 'inserted-value' );
 
-submit.addEventListener( 'click', function(e) {
+submit.addEventListener( 'submit', function(e) {
     e.preventDefault();
 
     var inputText = parseInt( textValue.value, 10 );
 
     if ( textValue.value === '' ) {
         alert('Put a Value between 1 to 200');
-    }
-
-    if ( inputText <= 0 ) {
-        alert( 'The Min Value is 1' );
-    }
-
-    if ( inputText > 0 && inputText <= 200 ) {
-        getValue.innerHTML = ('$' + textValue.value);
-    }
-
-    if ( inputText >= 201 ) {
-        alert( 'The Max Value is 200' );
     }
 }, false);
 
