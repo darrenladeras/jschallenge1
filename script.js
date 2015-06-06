4d5241be49015352d43a32503914eb3f30d9e48d
Darren <darren.laderas@ninthdesign.com>
2015-06-07 01:34:17 +0800

    revised logic for factor displaying

--- script.js
+++ script.js
@@ -1,51 +1,67 @@
-var text = document.getElementById( 'textValue' );
-var submit = document.getElementById( 'submitValue' );
-var getValue = document.getElementById( 'inserted-value' );
+(function(window, document) {
+    var text = document.getElementById( 'textValue' );
+    var submit = document.getElementById( 'submitValue' );
+    var valueWrapper = document.getElementById( 'value-wrapper' );
+    var form = document.getElementById('form-wrapper');
+    var insertedValue = document.createElement('div');
+    // console.log(form.children[1]);
 
-submit.addEventListener( 'click', function(darren) {
-    darren.preventDefault();
-
-    // Convertion of inputed string to integer
-    var inputText = parseInt( textValue.value, 10 );
-
-    if ( textValue.value === '' ) {
-        alert( '"NO INPUT DETECTED", Put a Value between 1 to 200' );
-    }
-
-    if ( inputText <= 0 ) {
-        alert( 'The Min Value is 1' );
-    }
-
-    if ( textValue.value > 0 && textValue.value <= 200 ) {
-        var getValue = document.getElementById( 'inserted-value' );
-        console.log(getValue);
-        getValue.innerHTML = '$' + textValue.value;
-    }
-
-    if ( textValue.value >= 201 ) {
-        alert( 'The Max Value is 200' );
-    }
-}, false);
-
-var valueWrapper = document.getElementById( 'value-wrapper' );
-var factors = valueWrapper.children[1];
-console.log(factors);
-var myArray = [10, 20, 30, 40, 50];
-
-// valueWrapper.appendChild(link);
-// var link = document.createElement( 'a' );
-var ul = document.createElement('ul');
-
-getValue.addEventListener( 'click', function(e) {
-    for ( var i = 0; i < myArray.length; i++ ) {
-        li = document.createElement('li');
-        a = document.createElement('a');
+    form.addEventListener( 'submit', function(e) {
         e.preventDefault();
-        a.innerHTML = myArray[i];
-        li.appendChild(a);
-        ul.appendChild(li);
-        factors.appendChild(ul);
+        console.log(e.type);
 
-        console.log(myArray[i]);
-    }
-}, false);
+        // Convertion of inputed string to integer
+        var inputText = parseInt( textValue.value, 10 );
+
+        if ( inputText <= 0 ) {
+            alert( 'The Min Value is 1' );
+        }
+
+        if ( textValue.value > 0 && textValue.value <= 200 ) {
+            valueWrapper.appendChild(insertedValue);
+            insertedValue.className = 'inserted-value';
+            insertedValue.innerHTML = '$' + textValue.value;
+        }
+
+        if ( textValue.value >= 201 ) {
+            alert( 'The Max Value is 200' );
+        }
+
+        if ( text.value === '' ) {
+            alert( '"NO INPUT DETECTED", Put a Value between 1 to 200' );
+        }
+    }, false);
+
+    var ul = document.createElement('ul');
+    var factors = document.createElement('div');
+    ul.id = 'list-of-factors';
+
+    valueWrapper.appendChild(factors);
+    factors.className = 'factors';
+
+// function getRandomArbitrary(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
+
+    var factorization = function (value, factor) {
+        return value - factor;
+    };
+
+    console.log(factorization(190, 10));
+
+    insertedValue.addEventListener( 'click', function() {
+        var list = document.getElementById('list-of-factors');
+
+        var getFactors = [];
+
+        for ( var i = 10; i < 200 + 10; i+=10 ) {
+            li = document.createElement('li');
+            getFactors.push(i)
+            li.innerHTML = '$' + i;;
+            ul.appendChild(li);
+            factors.appendChild(ul);
+        }
+
+        console.log(getFactors);
+    }, false);
+})(window, document);
